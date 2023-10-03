53e07f6fe2ae35b5846122623f479580bcdc5816
Hieutm-98 <146509434+Hieutm-98@users.noreply.github.com>
2023-10-03 09:33:56 +0700

    Update First_project.py

--- First_project.py
+++ First_project.py
@@ -1,1 +1,9 @@
-print ('Hello world')
+import streamlit
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from dotenv import load_dotenv
+
+
+
+st.title("Tỷ giá USD/VND tại Ngân hàng VCB giai đoạn 2014 - 2019")
