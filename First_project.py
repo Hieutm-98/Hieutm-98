4d290c6072191f521d661b7bbb1504e65b66fadd
Hieutm-98 <146509434+Hieutm-98@users.noreply.github.com>
2023-10-03 10:44:11 +0700

    Update First_project.py

--- First_project.py
+++ First_project.py
@@ -1,9 +1,37 @@
 import streamlit
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
-st.title("Tỷ giá USD/VND tại Ngân hàng VCB giai đoạn 2014 - 2019")
-st.header("Tỷ giá USD/VND là gì")
-st.text("Tỷ giá USD/VND là tỷ giá hối đoái giữa đồng Đô la Mỹ (USD) và đồng Việt Nam Đồng \n(VND). Nó cho biết giá trị của một đơn vị tiền tệ USD so với tiền tệ VND. Tỷ giá\nnày thường được sử dụng để đo lường giá trị tiền tệ và tính toán giá trị giao dịch\nquốc tế.Tỷ giá USD/VND thường biến đổi hàng ngày dựa trên thị trường hối đoái quốc\ntế và có thể ảnh hưởng đến nhiều khía cạnh của nền kinh tế, bao gồm giá cả hàng hóa,\nlãi suất, và việc nhập khẩu và xuất khẩu của một quốc gia.")
+st.title("Test ứng dụng chạy trên Streamlit với dữ liệu bất kỳ")
+st.header("Phân Tích Dữ Liệu Bán Hàng")
 
+@st.cache
+def load_data():
+    data = pd.read_csv("sales_data.csv")  # Thay đổi tên tệp CSV nếu cần
+    return data
 
+data = load_data()
+
+st.subheader("Dữ Liệu Bán Hàng")
+st.write(data)
+
+st.subheader("Biểu Đồ Trực Quan")
+chart_type = st.selectbox("Chọn loại biểu đồ:", ["Bar Chart", "Line Chart", "Scatter Plot"])
+
+if chart_type == "Bar Chart":
+    st.bar_chart(data)
+
+elif chart_type == "Line Chart":
+    st.line_chart(data)
+
+else:
+    st.scatter_chart(data)
+
+st.subheader("Phân Tích Kỹ Thuật")
+
+average_sales = data["Sales"].mean()
+total_sales = data["Sales"].sum()
+
+st.write(f"Trung bình doanh số bán hàng: ${average_sales:.2f}")
+st.write(f"Tổng doanh số bán hàng: ${total_sales:.2f}")
