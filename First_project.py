c57f18029f4f23adb1a0d4871a658a87fdb1409e
Hieutm-98 <146509434+Hieutm-98@users.noreply.github.com>
2023-10-03 09:57:23 +0700

    Update First_project.py

--- First_project.py
+++ First_project.py
@@ -2,3 +2,5 @@ import streamlit
 import streamlit as st
 
 st.title("Tỷ giá USD/VND tại Ngân hàng VCB giai đoạn 2014 - 2019")
+st.header("Tỷ giá USD/VND là gì")
+st.text("Tỷ giá USD/VND là tỷ giá hối đoái giữa đồng Đô la Mỹ (USD) và đồng Việt Nam Đồng \n(VND). Nó cho biết giá trị của một đơn vị tiền tệ USD so với tiền tệ VND. Tỷ giá\nnày thường được sử dụng để đo lường giá trị tiền tệ và tính toán giá trị giao dịch\nquốc tế.Tỷ giá USD/VND thường biến đổi hàng ngày dựa trên thị trường hối đoái quốc\ntế và có thể ảnh hưởng đến nhiều khía cạnh của nền kinh tế, bao gồm giá cả hàng hóa,\nlãi suất, và việc nhập khẩu và xuất khẩu của một quốc gia.")
