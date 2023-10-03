851f43262266e82624830ad612204cbd3ea5e437
Hieutm-98 <146509434+Hieutm-98@users.noreply.github.com>
2023-10-03 11:19:46 +0700

    Update First_project.py

--- First_project.py
+++ First_project.py
@@ -1,7 +1,6 @@
 import streamlit
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
 st.title("Test ứng dụng chạy trên Streamlit với dữ liệu bất kỳ")
 st.header("Phân Tích Dữ Liệu Bán Hàng")
