95468b8f9a16fa184a3798deda53f1fe9a71e432
Hieutm-98 <146509434+Hieutm-98@users.noreply.github.com>
2023-10-04 15:00:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-import streamlit
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-
 
 st.title("Tỷ giá USD/VND tại Ngân hàng VCB giai đoạn 2014 - 2019")
 st.header("Tỷ giá USD/VND là gì")
 st.text("Tỷ giá USD/VND là tỷ giá hối đoái giữa đồng Đô la Mỹ (USD) và đồng Việt Nam Đồng \n(VND). Nó cho biết giá trị của một đơn vị tiền tệ USD so với tiền tệ VND. Tỷ giá\nnày thường được sử dụng để đo lường giá trị tiền tệ và tính toán giá trị giao dịch\nquốc tế.Tỷ giá USD/VND thường biến đổi hàng ngày dựa trên thị trường hối đoái quốc\ntế và có thể ảnh hưởng đến nhiều khía cạnh của nền kinh tế, bao gồm giá cả hàng hóa,\nlãi suất, và việc nhập khẩu và xuất khẩu của một quốc gia.")
 
-duong_dan_excel = r'C:\Users\admin\Desktop\Tygia_USDVND.xlsx'
-
-data = pd.read_excel(duong_dan_excel)
+data = pd.read_excel('Tygia_USDVND.xlsx')
 data.index = data.index + 1
 data['Date'] = data['Date'].dt.date
 st.header('Bảng tỷ giá USD/VND tại Ngân hàng VCB giai đoạn 2014 - 2019')
@@ -55,5 +51,3 @@ with col2:
 
     else:
         st.write('Không có dữ liệu để hiển thị.')
-
-
