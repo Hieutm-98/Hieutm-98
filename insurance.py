7dc356a24abfdaeed972732615c6f6bbe6edb339
Hieutm-98 <146509434+Hieutm-98@users.noreply.github.com>
2023-10-06 11:13:27 +0700

    Update insurance.py

--- insurance.py
+++ insurance.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("Test app streamlit")
 st.header("Test app khi chỉ có text")
-st.text("Chỉ có text thì có thể không bị lỗi
+st.text("Chỉ có text thì có thể không bị lỗi")
 st.button("Predict Yes")
